ac6f0a755f37d5f75862c321070164b7cc4a3c21
yuhill <hill.julija@gmail.com>
2017-08-16 19:39:56 -0400

    8/16/2017
    Began writing function to normalize salaries

--- glassdoor_analysis/data_cleaner.py
+++ glassdoor_analysis/data_cleaner.py
@@ -1,2 +1,20 @@
-def example(l):
-    return [x+1 for x in l]
+from decimal import Decimal, ROUND_DOWN
+
+HRS_PR_WK = 40
+WKS_PR_YR = 52
+
+def normalize_salaries(input_salaries):
+    salary_data = list()
+    for line in input_salaries:
+        title, salary = line
+        salary_split = salary.split()
+        first_element = salary_split[0][1:].replace(',','')
+        if salary_split[1] == 'hourly':
+            yearly_salary = float(first_element) * HRS_PR_WK * WKS_PR_YR
+            salary_data.append(
+                (title, yearly_salary)
+            )
+        else:
+            salary_data.append(
+                (title, float(first_element)))
+    return salary_data
--- tests/test_data_cleaner.py
+++ tests/test_data_cleaner.py
@@ -1,5 +1,23 @@
-from glassdoor_analysis.data_cleaner import example
+from decimal import Decimal
+
+from glassdoor_analysis.data_cleaner import normalize_salaries
 
 
-def test_example():
-    assert [2, 3, 4] == example([1, 2, 3])
+def test_normalize_salaries():
+    start_data = [
+        ('Intern - Hourly', '$23.92 hourly'),
+        ('Financial Analyst ', '$62,164 per year'),
+        ('Senior Software Engineer ', 'About $83k - $119k'),
+        #('Project Manager/Computer and Info Systems Manager ', 'About $88k - $95k'),
+        #('SVT Engineer II - Hourly', 'About $33 - $35 hourly'),
+
+    ]
+
+    expected_restult = [
+        ('Intern - Hourly', 49753.600000000006),
+        ('Financial Analyst ', 62164),
+        (),
+        #(),
+        #(),
+    ]
+    assert normalize_salaries(start_data) == expected_restult
